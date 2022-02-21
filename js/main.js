18bf42d194f596f390d7d0afc3b69f223a86de05
Stanislav <stanislav.v.vasilyev@gmail.com>
2022-02-21 17:09:43 +0300

    рефакторинг генераторов случайных чисел

--- js/main.js
+++ js/main.js
@@ -1,53 +1,60 @@
 //The function generates a random integer number
 // with a value between the minNumber and the maxNumber inclusively.
 //Please pay attention:
-//the maxNumber must be greater or equal than the minNumber, else the function returns the UNDEFINED value
-function generateInteger(minNumber, maxNumber){
-  const min= +minNumber;
-  const max= +maxNumber;
-  if(max > min){
-    const amplitude = (max-min)*0.5;
-    const outcome = min+Math.round(amplitude*(1+Math.sin(Date.now())));
-    if(outcome>max){
-      return max;
+//the maxNumber must be greater or equal than the minNumber, else the function returns null
+function generateInteger(min, max){
+  const minNumber = Number(min);
+  const maxNumber = Number(max);
+  if(maxNumber > minNumber){
+    const amplitude = (maxNumber-minNumber)*0.5;
+    const currentTime = Date.now();
+    const offset = amplitude*Math.sin(currentTime);
+    const outcome = minNumber+Math.round(amplitude+offset);
+
+    if(outcome>maxNumber){
+      return maxNumber;
     }
-    if(outcome<min){
-      return min;
+    if(outcome<minNumber){
+      return minNumber;
     }
     return outcome;
   }
-  if(max === min){
-    return max;
+  if(maxNumber === minNumber){
+    return maxNumber;
   }
+  return null;
 }
 //The function generates a random floating point number
 // with a value between the minNumber and the maxNumber inclusively.
 // digits is the number of digits to appear after the decimal point
 //Please pay attention:
-//the maxNumber must be greater or equal than the minNumber, else the function returns the UNDEFINED value
-function generateFloat(minNumber, maxNumber, digits){
-  const min= +minNumber;
-  const max= +maxNumber;
-  if(max > min){
-    const amplitude = (max-min)*0.5;
-    let outcome = min+amplitude*(1+Math.sin(Date.now()));
+//the maxNumber must be greater or equal than the minNumber, else the function returns null
+function generateFloat(min, max, digits){
+  const minNumber = Number(min);
+  const maxNumber = Number(max);
+  if(maxNumber > minNumber){
+    const amplitude = (maxNumber-minNumber)*0.5;
+    const currentTime = Date.now();
+    const offset = amplitude*Math.sin(currentTime);
+    let outcome = minNumber+amplitude+offset;
     let scaler = 1;
 
     for(let i=0; i<digits;i++){
       scaler*=10;
     }
     outcome=Math.round(outcome*scaler)/scaler;
-    if(outcome>max){
-      return max;
+    if(outcome>maxNumber){
+      return maxNumber;
     }
-    if(outcome<min){
-      return min;
+    if(outcome<minNumber){
+      return minNumber;
     }
     return outcome;
   }
-  if(max === min){
-    return max;
+  if(maxNumber === minNumber){
+    return maxNumber;
   }
+  return null;
 }
 generateInteger(0,1);
 generateFloat(1.0001, 1.0002,7);
