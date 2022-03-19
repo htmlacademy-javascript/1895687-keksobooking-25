264b9ec58050f6526fef7fe8e996dabe1df64baf
Keks <keks@htmlacademy.ru>
2022-03-19 07:29:56 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/adverts-rendering.js
+++ 7/js/adverts-rendering.js
@@ -16,46 +16,47 @@ const theAdvertTemplate = document.querySelector('#card').content;
 const theAdvertSample = theAdvertTemplate.querySelector('.popup');
 const fragment = document.createDocumentFragment();
 
-adverts.forEach(( {author, offer} ) => {
+const fillAdvertElementWithData = (adElement, advertData) =>{
+  const { author, offer } = advertData;
   const { avatar } = author;
   const { title, address, price, type, rooms, guests, checkIn, checkOut, features, description, photos } = offer;
-  const newAdvert = theAdvertSample.cloneNode(true);
-  const titleElement = newAdvert.querySelector('.popup__title');
+
+  const titleElement = adElement.querySelector('.popup__title');
   titleElement.textContent = title ? title : hideElement(titleElement);
-  const addressElement = newAdvert.querySelector('.popup__text--address');
+  const addressElement = adElement.querySelector('.popup__text--address');
   addressElement.textContent = address ? address : hideElement(addressElement);
-  const priceElement = newAdvert.querySelector('.popup__text--price');
+  const priceElement = adElement.querySelector('.popup__text--price');
   priceElement.textContent = price ? `${ price } ₽/ночь` : hideElement(priceElement);
-  const typeElement = newAdvert.querySelector('.popup__type');
+  const typeElement = adElement.querySelector('.popup__type');
   typeElement.textContent = type ? thesaurus[type] : hideElement(typeElement);
   const visitors = guests ? guests : 0;
-  const capacityElement = newAdvert.querySelector('.popup__text--capacity');
+  const capacityElement = adElement.querySelector('.popup__text--capacity');
   capacityElement.textContent  = rooms ?
     `${ rooms } комнаты для ${ visitors } гостей` :
     hideElement(capacityElement);
-  const timeElement = newAdvert.querySelector('.popup__text--time');
+  const timeElement = adElement.querySelector('.popup__text--time');
   timeElement.textContent = checkIn && checkOut ?
     `Заезд после ${ checkIn }, выезд до ${ checkOut }` :
     hideElement(timeElement);
-  const featuresContainer = newAdvert.querySelector('.popup__features');
+  const featuresContainer = adElement.querySelector('.popup__features');
   if(features){
-    featuresContainer.querySelectorAll('.popup__feature').forEach((element) => {
+    featuresContainer.querySelectorAll('.popup__feature').forEach((unit) => {
       const isDeclared = features.some(
-        (feature) => element.classList.contains(`popup__feature--${feature}`)
+        (feature) => unit.classList.contains(`popup__feature--${feature}`)
       );
       if(!isDeclared){
-        element.remove();
+        unit.remove();
       }
     });
   }
   else{
     hideElement(featuresContainer);
   }
-  const descriptionElement = newAdvert.querySelector('.popup__description');
+  const descriptionElement = adElement.querySelector('.popup__description');
   descriptionElement.textContent = description ? description: hideElement(descriptionElement);
-  const photosContainer = newAdvert.querySelector('.popup__photos');
+  const photosContainer = adElement.querySelector('.popup__photos');
   const photoSample = photosContainer.querySelector('.popup__photo').cloneNode(true);
-  photosContainer.querySelectorAll('.popup__photo').forEach((element) => element.remove());
+  photosContainer.querySelectorAll('.popup__photo').forEach((unit) => unit.remove());
   if(photos){
     photos.forEach((photo) => {
       const newPhotoItem = photoSample.cloneNode(true);
@@ -66,9 +67,14 @@ adverts.forEach(( {author, offer} ) => {
   else{
     hideElement(photosContainer);
   }
-  newAdvert.querySelector('.popup__avatar').src = avatar ? avatar : '';
+  const avatarElement = adElement.querySelector('.popup__avatar');
+  avatarElement.src = avatar ? avatar : hideElement(avatarElement);
+};
 
-  fragment.append(newAdvert);
+adverts.forEach((advert) => {
+  const newAdvertElement = theAdvertSample.cloneNode(true);
+  fillAdvertElementWithData(newAdvertElement, advert);
+  fragment.append(newAdvertElement);
 });
 
 document.querySelector('#map-canvas').append(fragment);
