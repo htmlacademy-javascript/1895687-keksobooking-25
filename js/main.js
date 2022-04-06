bc87f518285d4311720d3c82ee0e7c94ce9b5c90
Stanislav <stanislav.v.vasilyev@gmail.com>
2022-04-07 02:45:00 +0300

    рефакторинг карты

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { setUserAdFormSubmit } from './user-ad-form-set-up.js';
-import { createMarkers, activateFiltersForm } from './user-map.js';
+import { createMarkers, activateFiltersForm, initialiseMap } from './user-map.js';
 import { getData } from './communication.js';
 import { showLoadErrorMessage } from './show-error.js';
 import { constructErrorElement, constructSuccessElement } from './user-ad-form-status-toggling.js';
@@ -7,6 +7,8 @@ import { getArrayCutTo } from './array-utils.js';
 import './ads-filter.js';
 
 const MARKERS_COUNT = 10;
+
+initialiseMap();
 
 getData(
   (data) => {
